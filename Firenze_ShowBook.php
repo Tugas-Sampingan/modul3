294214ea313deb3dc241b9356a95e8cccad4f434
dhanakbar <maulana261100@gmail.com>
2021-12-08 20:20:55 +0800

    home

--- Firenze_ShowBook.php
+++ Firenze_ShowBook.php
@@ -1,12 +1,16 @@
-<div class="d-flex flex-wrap px-5">
-    <?php foreach ($tabel as $row): ?>
-    <div class="card" style="width: 300px; margin-right: 2rem; margin-bottom:2rem;">
-        <img src="images/buku/<?= $row["gambar"]; ?>" class="card-img-top" alt="">
-        <div class="card-body">
-            <h5 class="card-title"><?= $row["judul_buku"]; ?></h5>
-            <p class="card-text"><?= $row["deskripsi"]; ?></p>
-            <a href="Firenze_detailBuku.php?id_buku=<?= $row["id_buku"]; ?>" class="btn btn-primary">Tampilkan Lebih Lanjut</a>
-        </div>
+<div class="container">
+    <div class="row">
+        <?php foreach ($tabel as $row) : ?>
+            <div class="col-6 col-md-4">
+                <div class="m-2 border">
+                    <img src="<?= 'buku/' . $row["gambar"]; ?>" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $row["judul_buku"]; ?></h5>
+                        <p class="card-text"><?= $row["deskripsi"]; ?></p>
+                        <a href="Firenze_detailBuku.php?id_buku=<?= $row["id_buku"]; ?>" class="btn btn-primary">Tampilkan Lebih Lanjut</a>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
-    <?php endforeach; ?>
 </div>
--- Firenze_home.php
+++ Firenze_home.php
@@ -1,41 +1,42 @@
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <title>HOME - MODUL3 FIRENZE</title>
-    </head>
 
-    <?php
-        include "Firenze_Connection.php";
-        $tabel = mysqli_query($koneksi, "SELECT * FROM buku_table");
-        $baris = mysqli_num_rows($tabel);
-    ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>HOME - MODUL3 FIRENZE</title>
+</head>
 
-    <body>
-        <script src= "js/bootsrap.js"></script>
-        <script src="js/popper.min.js"></script>
+<?php
+include "Firenze_Connection.php";
+$tabel = mysqli_query($conn, "SELECT * FROM buku_table");
+$baris = mysqli_num_rows($tabel);
+?>
 
-        <!-- navbar -->
-        <header>
+<body>
+    <script src="js/bootsrap.js"></script>
+    <script src="js/popper.min.js"></script>
+
+    <!-- navbar -->
+    <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div class="container-fluid">
-            <a class="navbar-brand" href="#"><img src="logo-ead.png" style="width: 130px;" alt=""></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <form class="my-auto" action="Firenze_tambahBuku.php">
-                <button class="btn btn-primary" type="submit">Tambah Buku</button>
-            </form>
+                <a class="navbar-brand" href="#"><img src="logo-ead.png" style="width: 130px;" alt=""></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <form class="my-auto" action="Firenze_tambahBuku.php">
+                    <button class="btn btn-primary" type="submit">Tambah Buku</button>
+                </form>
             </div>
         </nav>
-        </header>
+    </header>
 
-        <br><br><br><br><br><br><br><br><br><br><br>
+    <br><br><br><br><br><br><br><br><br><br><br>
 
-         <!-- CEK BUKU -->
+    <!-- CEK BUKU -->
 
-        <main class="py-3">
+    <main class="py-3">
         <?php
         if ($baris > 0) {
             include "Firenze_ShowBook.php";
@@ -43,22 +44,23 @@
             include "Firenze_NoBooks.php";
         }
         ?>
-        </main>
+    </main>
 
-        <br><br><br><br><br>
+    <br><br><br><br><br>
 
-        <!-- footer -->
-        <footer class="mt-4">
+    <!-- footer -->
+    <footer class="mt-4">
         <div class="container-fullwidth">
             <br><br><br>
             <div class="jumbotron" id="home" style="text-align: center;">
-            <div class="container"></div>
-            <div class="text-center">
-            <img src="logo-ead.png" alt="logo" width="200" height="80">
-            <h6 class="display-5"><b>Perpustakaan EAD</b></h6>
-            <p>©Firenze_1202194019</p>
+                <div class="container"></div>
+                <div class="text-center">
+                    <img src="logo-ead.png" alt="logo" width="200" height="80">
+                    <h6 class="display-5"><b>Perpustakaan EAD</b></h6>
+                    <p>©Firenze_1202194019</p>
+                </div>
             </div>
-        </div>
-        </footer>
-    </body>
+    </footer>
+</body>
+
 </html>
